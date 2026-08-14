d94af091b8e3edc5350cd718d219b53a5aa183e6
Lovish Dhingra <84726563+LovishDhingra@users.noreply.github.com>
2026-08-14 23:50:12 +0530

    Add files via upload

---
+++ 02_reverse_linked_list.cpp
@@ -0,0 +1,44 @@
+// Problem: Reverse a Singly Linked List
+// Approach: Iterative pointer reversal using prev/curr/next pointers.
+// Time: O(n)   Space: O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* reverseList(ListNode* head) {
+    ListNode* prev = nullptr;
+    ListNode* curr = head;
+    while (curr) {
+        ListNode* nxt = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nxt;
+    }
+    return prev; // new head
+}
+
+void printList(ListNode* head) {
+    while (head) {
+        cout << head->val << " -> ";
+        head = head->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+
+    cout << "Original: "; printList(head);
+    head = reverseList(head);
+    cout << "Reversed: "; printList(head);
+    return 0;
+}
